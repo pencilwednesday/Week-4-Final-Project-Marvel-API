5f24e4ab8f45d8612e1f3a5394ef1e0b5d538b0d
pencilwednesday <112817872+pencilwednesday@users.noreply.github.com>
2024-12-09 12:24:29 -0800

    save repo

---
+++ index.js
@@ -0,0 +1,7 @@
+// Here's your personal Marvel Comics API information:
+
+// Your public key
+// b5025002fed92c379235ccd6df1878c1
+// Your private key
+// c594216bf76f5a1792d5e0ad5842d46d743b8832
+// Read more about how to use your keys to sign requests.
